a429b8f101736073590d96bd3cdc27b88cd74ef7
vfarah-if <vincent.farah@codurance.com>
2019-03-20 05:33:47 +0000

    Fixed failing test for the party convertion

--- src/ElectionKata/ElectionResultsConverter.cs
+++ src/ElectionKata/ElectionResultsConverter.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using static ElectionKata.ErrorMessages;
 
 namespace ElectionKata
 {
     public class ElectionResultsConverter
     {
+        private readonly Dictionary<string, string> partyCodes;
+
+        public ElectionResultsConverter()
+        {
+            this.partyCodes = new Dictionary<string, string>()
+            {
+                { "C", "Conservative Party" }
+            };
+        }
         public string Convert(string electionData)
         {
             if (string.IsNullOrWhiteSpace(electionData))
@@ -13,7 +23,7 @@ namespace ElectionKata
             }
 
             var result = electionData.Split(",", StringSplitOptions.RemoveEmptyEntries);
-            return $"{result[0]} ||";
+            return $"{result[0]} || {partyCodes[result[2].Trim()]}";
         }
     }
 }
