13c9b420dde472f3d258adc2b3227169caa06d19
vfarah-if <sefrica@live.com>
2019-03-21 15:17:13 +0000

    Fixed small indentation

--- tests/ElectionKataTests/UnitTests/ElectionResultsConverterShould.cs
+++ tests/ElectionKataTests/UnitTests/ElectionResultsConverterShould.cs
@@ -55,6 +55,7 @@ namespace ElectionKataTests.UnitTests
                         "Islington South & Finsbury, 22547, L";
             var expected = $"Cardiff West || Conservative Party | 100.00%" + NewLine +
                            "Islington South & Finsbury || Labour Party | 100.00%" + NewLine;
+
             var actual = electionResultsConverter.Convert(input);
 
             actual.Should().Be(expected);
